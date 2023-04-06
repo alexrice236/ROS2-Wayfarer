ce3ad1350d8017b4b1bad16a3516a4ee91a681f1
Alex Rice <alexrice826@gmail.com>
2023-04-06 00:58:54 -0400

    structure of workspace

---
+++ src/my_computer/my_computer_goal/my_computer_goal/goal_pub_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from my_computer_goal.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/my_computer/my_computer_mocap/my_computer_mocap/mocap_pub_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from my_computer_mocap.')
+
+
+if __name__ == '__main__':
+    main()
