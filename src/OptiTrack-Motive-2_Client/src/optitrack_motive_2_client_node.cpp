cf56ffe4aac73688e9a28c894d7399345f189d3d
Alex Rice <alexrice826@gmail.com>
2023-04-23 14:45:07 -0400

    viconstate

--- src/OptiTrack-Motive-2_Client/src/optitrack_motive_2_client_node.cpp
+++ src/OptiTrack-Motive-2_Client/src/optitrack_motive_2_client_node.cpp
@@ -120,7 +120,7 @@ int main(int argc, char *argv[])
       // Get past state and publisher (if they exist)
       bool hasPreviousMessage = (rosPublishers.find(mocap_packet.rigid_body_id) != rosPublishers.end());
       // create Publisher object
-      rclcpp::Publisher<acl_msgs::msg::ViconState>::SharedPtr publisher;
+      rclcpp::Publisher<acl_msgs::msg::ViconState> publisher;
       acl_msgs::msg::ViconState lastState;
       acl_msgs::msg::ViconState currentState;
 
@@ -128,11 +128,11 @@ int main(int argc, char *argv[])
       if (!hasPreviousMessage){
         std::string topic = "/" + mocap_packet.model_name + "/vicon";
         // specify publisher topic and message type
-        auto publisher = node->create_publisher<acl_msgs::msg::ViconState>::SharedPtr(topic, 1);
-        rosPublishers[mocap_packet.rigid_body_id] = publisher;
+        auto publisher = node->create_publisher<acl_msgs::msg::ViconState>(topic, 1);
+        rosPublishers[mocap_packet.rigid_body_id] = &publisher;
       } else {
         // Get saved publisher and last state
-        publisher = rosPublishers[mocap_packet.rigid_body_id];
+        publisher = *rosPublishers[mocap_packet.rigid_body_id];
         lastState = pastStateMessages[mocap_packet.rigid_body_id];
       }
 
