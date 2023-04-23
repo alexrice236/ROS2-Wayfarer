1bc7b123a9aca7ecf7166ede15970980410edb52
Alex Rice <alexrice826@gmail.com>
2023-04-23 16:40:45 -0400

    topic

--- src/OptiTrack-Motive-2_Client/src/optitrack_motive_2_client_node.cpp
+++ src/OptiTrack-Motive-2_Client/src/optitrack_motive_2_client_node.cpp
@@ -192,7 +192,7 @@ int main(int argc, char *argv[])
       pastStateMessages[mocap_packet.rigid_body_id] = currentState;
       // Publish ROS state.
 
-      *publisher->publish(currentState);
+      publisher->publish(currentState);
 
     }
   }
